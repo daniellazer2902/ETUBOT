a9878b00c2b7fc0bd4d3579c492956861f98a718
daniellazer2902 <52815463+daniellazer2902@users.noreply.github.com>
2020-03-04 17:56:19 +0100

    Add files via upload

---
+++ daniel/jerry.py
@@ -0,0 +1,21 @@
+
+import discord
+from discord.ext import commands
+
+guild = 682156076342312960
+
+bot = commands.Bot(command_prefix='$')
+
+@bot.command()
+async def test(ctx, pseudo, temps, *, reason):
+    await ctx.send(arg)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(':ok_hand:')
+
+
+bot.run('Njg0NzYxOTE1NTg2OTA0MDg1.Xl-6dw.3JBjpsmyBAvynreD2oBPdsKa9no')
+
+
+
