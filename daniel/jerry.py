7cfd904210abf98bc68a766a43c61c35b2da6495
daniellazer2902 <52815463+daniellazer2902@users.noreply.github.com>
2020-03-11 00:02:25 +0100

    retrieve last news

--- daniel/jerry.py
+++ daniel/jerry.py
@@ -1,21 +1,41 @@
 
 import discord
 from discord.ext import commands
+import json
+import requests
+import bs4 as BeautifulSoup
 
-guild = 682156076342312960
+
 
 bot = commands.Bot(command_prefix='$')
-
-@bot.command()
-async def test(ctx, pseudo, temps, *, reason):
-    await ctx.send(arg)
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send(':ok_hand:')
+guild = 682156076342312960
 
 
-bot.run('Njg0NzYxOTE1NTg2OTA0MDg1.Xl-6dw.3JBjpsmyBAvynreD2oBPdsKa9no')
+
+@bot.event
+async def on_ready():
+    print('Je suis co !')
+
+#@bot.command()
+#async def text(ctx, pseudo, temps, *, reason):
+#    await ctx.send(reason)
+
+#@bot.command()
+#async def ping(ctx):
+#    await ctx.send(':ok_hand:')
+
+
+
+    
+html = requests.get('https://overwatch.judgehype.com/')
+context = html.text
+soup = BeautifulSoup.BeautifulSoup(context, "html.parser")
+
+for link in soup.find_all(class_="gallerynobind", limit=1):
+    last_thread = link.get('href')
+print(last_thread)
+
+bot.run('Njg0NzYxOTE1NTg2OTA0MDg1.XmfVVg.AU94SP045cs02ibx50vB3G6IbWc')
 
 
 
