c6c0a7b95cdad1a1ffc961f0eef797199bf93d41
daniellazer2902 <52815463+daniellazer2902@users.noreply.github.com>
2020-03-11 13:00:17 +0100

    Newsletter

--- daniel/jerry.py
+++ daniel/jerry.py
@@ -4,6 +4,8 @@ from discord.ext import commands
 import json
 import requests
 import bs4 as BeautifulSoup
+import os
+import time
 
 
 
@@ -12,9 +14,6 @@ guild = 682156076342312960
 
 
 
-@bot.event
-async def on_ready():
-    print('Je suis co !')
 
 #@bot.command()
 #async def text(ctx, pseudo, temps, *, reason):
@@ -26,16 +25,73 @@ async def on_ready():
 
 
 
-    
-html = requests.get('https://overwatch.judgehype.com/')
-context = html.text
-soup = BeautifulSoup.BeautifulSoup(context, "html.parser")
 
-for link in soup.find_all(class_="gallerynobind", limit=1):
-    last_thread = link.get('href')
-print(last_thread)
+@bot.event
+async def on_ready():
+    print('Je suis co !')
 
-bot.run('Njg0NzYxOTE1NTg2OTA0MDg1.XmfVVg.AU94SP045cs02ibx50vB3G6IbWc')
+
+
+
+
+    starttime=time.time()
+    while True:
+
+
+        #================================================
+        #          RECUPERATION DU DERNIER ARTICLE
+        #================================================
+
+                #============================================
+                #           obtention d'un article
+                #============================================
+
+        lien = 'https://www.01net.com/actualites/applis-logiciels/'
+        mon_selector = "table-cell-middle padding-inside-all"
+
+        html = requests.get(lien)
+        context = html.text
+        soup = BeautifulSoup.BeautifulSoup(context, "html.parser")
+
+        for link in soup.find_all(class_=mon_selector, limit=1):
+            last_thread = link.get('href')
+            #print(last_thread)
+
+                #============================================
+                #      filtrage du lien pour avoir www.*
+                #============================================
+
+        good = True
+        while good == True:
+            if last_thread.startswith('www.'):
+                good = False
+
+            else:
+                last_thread = last_thread[1:]
+
+        print(last_thread)
+
+                #============================================
+                #      comparaison avec l'ancien article
+                #============================================
+
+
+        art_r = open('./article_appli.txt', 'r')
+        content = art_r.read()
+        art_r.close()
+
+        if last_thread != content:
+            art_w = open('./article_appli.txt', 'w')
+            art_w.write(last_thread)
+            art_w.close() 
+            print('Un article a ajoute')
+            await bot.get_channel(687254822428475412).send('https://'+last_thread)
+        else:
+            print('Aucun article recent')
+
+        time.sleep(5 - time.time() % 5)
+
+bot.run('')
 
 
 
