bf5a2c2239ef0e52330e7a413d711753dae0ea84
hugo95470 <59528575+hugo95470@users.noreply.github.com>
2020-04-24 11:58:30 +0200

    Add files via upload

---
+++ etud_bot.py
@@ -0,0 +1,22 @@
+import os
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='.')
+
+@bot.command()
+async def load(ctx, extension):
+    bot.load_extension(f'etubot_extension.{extension}')
+    
+@bot.command()
+async def unload(ctx, extension):
+    bot.unload_extension(f'etubot_extension.{extension}')
+
+if __name__ == "__main__":
+    
+    for filename in os.listdir('./etubot_extension'):
+        if filename.endswith('.py'):
+            bot.load_extension(f'etubot_extension.{filename[:-3]}')
+
+
+bot.run("NjgyMjIxNDAxOTE0MzQzNDQ0.XqC8-A.kWks2__z0-nAQ6peJgbFVd8CzwA")#sprit
+#bot.run("NjgyMjIxNDAxOTE0MzQzNDQ0.Xm9YHQ.PxYpZyCm7-EsQ_sHXGmBqsxjtvA")
