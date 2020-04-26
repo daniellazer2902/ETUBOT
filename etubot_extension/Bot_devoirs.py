f5ea1169a1c7783c9aa15dd28d72717cb28b8687
hugo95470 <59528575+hugo95470@users.noreply.github.com>
2020-04-26 18:43:23 +0200

    Add files via upload

--- etubot_extension/Bot_devoirs.py
+++ etubot_extension/Bot_devoirs.py
@@ -2,8 +2,8 @@ import discord
 from discord.ext import commands
 from tinydb import TinyDB, Query
 
-TOKEN = 'NjgyMTgyMTQ2ODUyNzgyMDgy.XqRM_g.K-x8U4OoLBuxTDkhdDd1mnn-nc8'
-bot = commands.Bot(command_prefix =';')
+#TOKEN = ''
+#bot = commands.Bot(command_prefix =';')
 
 
 db_devoir = TinyDB("db_devoir.json")
@@ -22,77 +22,83 @@ for item in db_devoir:
     print(item)
 
 
-@bot.event
-async def on_ready():
-    print('Je suis co !') #Affiche quand le bot est co !
+class bot_marine(commands.Cog):
 
-@bot.event
-async def db_ready():
-    print(db_devoir) #Affiche les données présente dans la base de donnée db_devoir
+    def __init__(self, client):
+        self.client = client
 
-@bot.command(brief="liste tout les devoirs")
-async def list_dev(ctx): 
-    all_dev = db_devoir.all()
-    for dev in all_dev :
-        await ctx.send(dev) #Affiche tout les devoirs présents dans la base de donnée
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Je suis co !') #Affiche quand le bot est co !
 
-@bot.command(brief="search_matiere [matiere]")
-async def search_matiere(ctx, mat_search):
-    all_dev = db_devoir.all()
-    verif = False
-    for dev in all_dev :
-        if dev.get('matiere').casefold() == mat_search.casefold() :
-            verif = True
+    @commands.Cog.listener()
+    async def db_ready(self):
+        print(db_devoir) #Affiche les données présente dans la base de donnée db_devoir
 
-            embed = discord.Embed(title="ETUBOT", description="Meilleur BOT de l'EPSI", color=0xeee657) #Permet d'obtenir une plus belle présentation des devoirs 
-            embed.add_field(name="DATE", value= dev.get('date'))
-            embed.add_field(name="MATIERE", value= dev.get('matiere'))
-            embed.add_field(name="DEVOIRS A FAIRE", value= dev.get('devoir'))
+    @commands.command(brief="liste tout les devoirs")
+    async def list_dev(self, ctx): 
+        all_dev = db_devoir.all()
+        for dev in all_dev :
+            await ctx.send(dev) #Affiche tout les devoirs présents dans la base de donnée
 
-            await ctx.author.send(embed = embed)
+    @commands.command(brief="search_matiere [matiere]")
+    async def search_matiere(self, ctx, mat_search):
+        all_dev = db_devoir.all()
+        verif = False
+        for dev in all_dev :
+            if dev.get('matiere').casefold() == mat_search.casefold() :
+                verif = True
 
-    if verif == False :
-        await ctx.send("Pas de devoirs ! Veuillez verifier l'orthographe !") #Affiche précisement les devoirs recherchés dans une matière en particulier et dis si ce n'est pas trouvable
+                embed = discord.Embed(title="ETUBOT", description="Meilleur BOT de l'EPSI", color=0xeee657) #Permet d'obtenir une plus belle présentation des devoirs 
+                embed.add_field(name="DATE", value= dev.get('date'))
+                embed.add_field(name="MATIERE", value= dev.get('matiere'))
+                embed.add_field(name="DEVOIRS A FAIRE", value= dev.get('devoir'))
 
-@bot.command()
-async def search_date(ctx, date_search):
-    all_dev = db_devoir.all()
-    verif = False
-    for dev in all_dev :
-        if dev.get('date') == date_search :
-            verif = True
+                await ctx.author.send(embed = embed)
 
-            embed = discord.Embed(title="ETUBOT", description="Meilleur BOT de l'EPSI", color=0xeee657) #Permet d'obtenir une plus belle présentation des devoirs 
+        if verif == False :
+            await ctx.send("Pas de devoirs ! Veuillez verifier l'orthographe !") #Affiche précisement les devoirs recherchés dans une matière en particulier et dis si ce n'est pas trouvable
 
-            embed.add_field(name="DATE", value= dev.get('date'))
+    @commands.command()
+    async def search_date(self, ctx, date_search):
+        all_dev = db_devoir.all()
+        verif = False
+        for dev in all_dev :
+            if dev.get('date') == date_search :
+                verif = True
 
-            embed.add_field(name="MATIERE", value= dev.get('matiere'))
-            
-            embed.add_field(name="DEVOIRS A FAIRE", value= dev.get('devoir')) 
+                embed = discord.Embed(title="ETUBOT", description="Meilleur BOT de l'EPSI", color=0xeee657) #Permet d'obtenir une plus belle présentation des devoirs 
 
-            await ctx.author.send(embed = embed)
+                embed.add_field(name="DATE", value= dev.get('date'))
 
-    if verif == False :
-        await ctx.send("Pas de devoirs pour cette date ! Veuillez verifier l'orthographe !") #Affiche précisement les devoirs recherchés pour une date en particulier et dis si il n'y a pas de devoirs à cette date
+                embed.add_field(name="MATIERE", value= dev.get('matiere'))
+                
+                embed.add_field(name="DEVOIRS A FAIRE", value= dev.get('devoir')) 
 
-@bot.command()
-async def enter_dev(ctx, date, matiere, devoir):
-    db_devoir.insert({'date': date, 'matiere': matiere, 'devoir': devoir})
-    await ctx.send("Vous venez de rajouter pour le {} \n {} : {} ".format(date, matiere, devoir)) #Commande qui permet de rentrer les devoirs sur discord pour la base de donnée
+                await ctx.author.send(embed = embed)
 
-@bot.command()
-async def rem_dev(ctx, date, mat_search):
+        if verif == False :
+            await ctx.send("Pas de devoirs pour cette date ! Veuillez verifier l'orthographe !") #Affiche précisement les devoirs recherchés pour une date en particulier et dis si il n'y a pas de devoirs à cette date
 
-    all_dev = db_devoir.all()
-    for dev in all_dev:
+    @commands.command()
+    async def enter_dev(self, ctx, date, matiere, devoir):
+        db_devoir.insert({'date': date, 'matiere': matiere, 'devoir': devoir})
+        await ctx.send("Vous venez de rajouter pour le {} \n {} : {} ".format(date, matiere, devoir)) #Commande qui permet de rentrer les devoirs sur discord pour la base de donnée
 
-        if dev.get('matiere').casefold() == mat_search.casefold() :
-            mat_search = dev.get('matiere')
-            db_devoir.remove((req.date == date) & (req.matiere == mat_search))
-            await ctx.send('Vous venez de supprimer les devoirs du {} en {}'.format(date, mat_search)) #Retire des devoirs de la base de donnée en fonction de la date et de la matière
-            break
+    @commands.command()
+    async def rem_dev(self, ctx, date, mat_search):
 
+        all_dev = db_devoir.all()
+        for dev in all_dev:
 
+            if dev.get('matiere').casefold() == mat_search.casefold() :
+                mat_search = dev.get('matiere')
+                db_devoir.remove((req.date == date) & (req.matiere == mat_search))
+                await ctx.send('Vous venez de supprimer les devoirs du {} en {}'.format(date, mat_search)) #Retire des devoirs de la base de donnée en fonction de la date et de la matière
+                break
 
-bot.run(TOKEN)
+def setup(client):
+    client.add_cog(bot_marine(client))
+
+#bot.run(TOKEN)
 
