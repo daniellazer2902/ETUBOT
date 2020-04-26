fde5d6fbfb6aca07cc681bb7371f381524467158
Monkey D Tanguy <61501393+OlivierTanguy@users.noreply.github.com>
2020-04-26 16:13:10 +0200

    Update bot_rappel.py

--- etubot_extension/bot_rappel.py
+++ etubot_extension/bot_rappel.py
@@ -32,11 +32,15 @@ def recup_message_edt(jour, ref_firstName, ref_lastName):
     url += str(my_date.day)
     url +="-20?token=imasecret&firstname=" + ref_firstName + "&lastname=" + ref_lastName
         
+    print(url)
+
     requete = requests.get(url)
         
     data =requete.content
         
     json_data=json.loads(data)
+
+    print("json_data : "+str(json_data))
         
     #remplacer le nombre par le jour
     if jour == 'ajd':
@@ -57,15 +61,20 @@ def recup_message_edt(jour, ref_firstName, ref_lastName):
     if 11 < my_date.hour < 14:
         heure_actuelle = "14:00" 
 
-    if 14 < my_date.hour < 16:
+    if 14 < my_date.hour < 18:
         heure_actuelle = "16:00" 
 
-    if my_date.hour >= 17:
+    if my_date.hour >= 19:
         heure_actuelle = "09:00" 
 
     for semaine in json_data['week'][jour]:
         if semaine["start"] == heure_actuelle:
             message += "De "+semaine["start"]+" à "+semaine["end"]+" : \t\t"+ semaine["subject"]+" en **" + semaine["room"]+ "**\n"
+    print("message : "+str(message))
+
+    if not message:
+        message="la journée est terminée"
+
     return message
 
 
@@ -77,24 +86,23 @@ class my_bot (commands.Cog):
 
 
     @commands.command(brief="Envoyer l'emploi du temps sur le tchat")
-    async def edt(self,ctx):
-        message = recup_message_edt("mercredi","olivier","tanguy")
-        print(message)
+    async def rappel(self,ctx, jour):
+        message = recup_message_edt(jour,"olivier","tanguy")
+        print("message : "+message)
         await ctx.send(message)
 
 
 
     @commands.command(brief="Envoyer l'emploi du temps en DM à l auteur")
-    async def mon_edt(self,ctx):
-        message = recup_message_edt("mercredi","olivier","tanguy")
+    async def mon_rappel(self,ctx, jour):
+        message = recup_message_edt(jour,"olivier","tanguy")
         await ctx.author.send(message)    
      
 
-
     @commands.command(brief="Envoyer l'emploi du temps en DM à l ou plusieurs membres ")
-    async def my_edt(self,ctx,destinataire):
+    async def mes_rappels(self,ctx,jour, destinataire):
 
-        message = recup_message_edt("mercredi","olivier","tanguy")    
+        message = recup_message_edt(jour,"olivier","tanguy")    
     
         # cherche si l'utisateur rentré existe ou non
         # si oui, lui envoie un mp avec les salles
@@ -103,9 +111,8 @@ class my_bot (commands.Cog):
         if destinataire == "all" :
             print("à tous")
 
-            #for member in ctx.guild.members:
-            #    await member.send(message)
-            await ctx.send(message)
+            for member in ctx.guild.members:
+                await member.send(message)
 
 
         else:
